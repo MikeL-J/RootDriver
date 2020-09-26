a398808bc3e15d8684adcca27013e89b49a8c79d
Michael J Smith <msmit22@gmail.com>
2020-09-26 12:58:32 -0400

    Refactor of object building

--- src/fileProcessor.js
+++ src/fileProcessor.js
@@ -13,8 +13,35 @@ const getData = (fileName) => {
     console.log('\x1b[35m', data, '\x1b[0m')
     const records = data.toString().split("\n");
     console.log('\x1b[36m', records, '\x1b[0m')
-    calculateRoutes(records)
+
+    let keys = ['type', 'driverName', 'startTime', 'endTime', 'miles'];
+    const delimiter = ' '
+
+    let values = records.map(r => {
+        return r.split(delimiter)
+      })
+
+    let json = values.map(v => {
+        return v.map((obj,index) => {
+          var myobj = {}
+          myobj[keys[index]] = obj
+          return myobj
+        })
+      })
+
+    json = json.map(r => {
+      return r.reduce((result, item) => {
+        var key = Object.keys(item)[0]
+        result[key] = item[key]
+        return result
+      }, {})
+    })
+
+      console.log(json)
   })
+
+  //calculateRoutes(json)
+
 }
 
 module.exports = {
